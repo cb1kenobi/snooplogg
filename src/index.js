bdf27c339e1ae7565c53cdcbe4e5941bbfafa190
Chris Barber <chris@cb1inc.com>
2017-11-15 06:20:33 -0600

    Fixed whitespace.

--- src/index.js
+++ src/index.js
@@ -785,11 +785,11 @@ function format(msg) {
  * ANSI color sequence regex.
  * @type {RegExp}
  */
- const pattern = [
- 	'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\\u0007)',
- 	'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))'
- ].join('|');
- const stripRegExp = new RegExp(pattern, 'g');
+const pattern = [
+	'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\\u0007)',
+	'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))'
+].join('|');
+const stripRegExp = new RegExp(pattern, 'g');
 
 /**
  * Transform stream that strips colors from the logger to the next stream in the
