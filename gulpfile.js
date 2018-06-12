e73c46987c5e8319b8fffc38a4a1c678a781bced
Chris Barber <chris@cb1inc.com>
2018-06-12 01:47:16 -0500

    Fixed URL in generated docs.

--- gulpfile.js
+++ gulpfile.js
@@ -52,8 +52,8 @@ gulp.task('docs', ['lint-src', 'clean-docs'], () => {
 					brand: {
 						title:       manifest.name,
 						description: manifest.description,
-						respository: 'https://github.com/cb1kenobi/cli-kit',
-						site:        'https://github.com/cb1kenobi/cli-kit'
+						respository: 'https://github.com/cb1kenobi/snooplogg',
+						site:        'https://github.com/cb1kenobi/snooplogg'
 					}
 				}
 			},
