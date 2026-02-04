7432ff9d09616ea3877eb4e350cdbe0515d8e654
Chris Barber <chris@cb1inc.com>
2026-02-04 00:24:43 -0600

    Update deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,26 +1,11 @@
-import replace from '@rollup/plugin-replace';
-import { readFileSync } from 'node:fs';
 import { defineConfig, type UserConfig } from 'tsdown';
 
-const { version } = JSON.parse(readFileSync('./package.json', 'utf8'));
-
 const config: UserConfig = defineConfig({
-	external: [
-		'msgpackr',
-		'ordered-binary',
-	],
 	entry: './src/index.ts',
 	format: ['es', 'cjs'],
-	minify: !process.env.SKIP_MINIFY,
+	inlineOnly: ['ansi-styles'],
+	minify: true,
 	platform: 'node',
-	plugins: [
-		replace({
-			preventAssignment: true,
-			values: {
-				'ROCKSDB_JS_VERSION': version,
-			},
-		}),
-	],
 	tsconfig: './tsconfig.build.json',
 });
 export default config;
