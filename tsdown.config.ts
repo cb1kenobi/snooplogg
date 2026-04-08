fc8436c4cf47457b8bc96ee8098f994271d1fe1a
Chris Barber <chris@cb1inc.com>
2026-04-08 15:56:02 -0500

    Update deps, fix TypeScript 6 and tsdown configs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig, type UserConfig } from 'tsdown';
 
 const config: UserConfig = defineConfig({
+	deps: {
+		onlyBundle: ['ansi-styles'],
+	},
 	entry: './src/index.ts',
 	format: ['es', 'cjs'],
-	inlineOnly: ['ansi-styles'],
 	minify: true,
 	platform: 'node',
 	tsconfig: './tsconfig.build.json',
 });
+
 export default config;
